71c9ee71533a2f1d3f25c9dda076791a0211532c
Dagaya_27 <supunlakshan1125@gmail.com>
2024-09-25 14:58:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DT43XD6B#1vdKIPKAIIL5PcW16aRdg8jVne2gQX92VKPm-lOfGcs",
+SESSION_ID: process.env.SESSION_ID || "Yz82XZKY#kSNIH9aQsQDJz-mBWjdDZIvG8xTn_WgQgHSY2L72gbQ",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
