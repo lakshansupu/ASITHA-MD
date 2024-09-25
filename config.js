7cf7ab21750c32debebcae31a11841bc84c532fd
Dagaya_27 <supunlakshan1125@gmail.com>
2024-09-26 03:29:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Yz82XZKY#kSNIH9aQsQDJz-mBWjdDZIvG8xTn_WgQgHSY2L72gbQ",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
+MONGODB: process.env.MONGODB || "mongodb+srv://supnu:shan27@cluster0.dw38v.mongodb.net/",//enter mongo db url
 };
