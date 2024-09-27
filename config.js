904e40355b9db4dc44d9973abc6df6903c2191a9
lakshansupu <sl1285269@gmail.com>
2024-09-27 21:55:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Yz82XZKY#kSNIH9aQsQDJz-mBWjdDZIvG8xTn_WgQgHSY2L72gbQ",
+SESSION_ID: process.env.SESSION_ID || "c6F2nDRQ#F-cQ7BG0Q9o3B6yERarVrnPZI6b8SCn7iOfOh0aDje4",
 MONGODB: process.env.MONGODB || "mongodb+srv://supnu:shan27@cluster0.dw38v.mongodb.net/",//enter mongo db url
 };
